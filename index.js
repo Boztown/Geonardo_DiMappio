8f8d5ae8367f1c9e5f1732ba01ebc33edbadbc6b
Ryan Bosinger <rbosinger@gmail.com>
2021-08-27 22:36:46 -0700

    Setup Leaflet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
+window.onload = () => {
+  let map = L.map('map').setView([51.505, -0.09], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+};
