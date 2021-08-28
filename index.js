3dc06af4103a494b719f7bb197d8ca5c0b88377c
Ryan Bosinger <rbosinger@gmail.com>
2021-08-28 13:32:16 -0700

    Add ability to click map and send point through server, then telnet

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import axios from "axios";
+
+async function postPoint(coord) {
+  try {
+    const response = await axios.post("/", coord);
+    console.log("axios res: ", response);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 window.onload = () => {
-  let map = L.map('map').setView([51.505, -0.09], 15);
+  let map = L.map("map").setView([48.45521397711524, -123.38275390554121], 16);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  map.on("click", function (e) {
+    // driver.geometry.coordinates[0] = e.latlng.lng;
+    // driver.geometry.coordinates[1] = e.latlng.lat;
+    console.log(e);
+    postPoint({ lon: e.latlng.lng, lat: e.latlng.lat });
+  });
 };
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 1235
+"use strict";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const port = 1235;
+const Telnet = require("telnet-client");
+
+const net = require("net");
+const socket = new net.Socket();
+socket.on("error", (err) => console.log(err));
+socket.on("data", (data) => console.log(data.toString()));
+socket.connect(5554, "127.0.0.1", () => {
+  socket.write("auth 3Y3NFY89DIIDK5DB\r\n");
+});
+
+app.use(express.static("dist"));
+app.use(express.json());
+
+app.post("/", function (req, res) {
+  console.log("body:", req.body);
+  socket.write(`geo fix ${req.body.lon} ${req.body.lat}\r\n`);
+  res.send("Got a POST request");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
