d97af5349219e34625c6736f340ca411b947c735
Ryan Bosinger <rbosinger@gmail.com>
2025-11-04 15:39:58 -0800

    Use Zod for schema validation

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import { z } from "zod";
 import { config } from "./config";
 import { TelnetSocket } from "./socket";
 
@@ -10,14 +11,20 @@ app.use(cors());
 
 const socket = new TelnetSocket();
 
+const setCoordsRequestSchema = z.object({
+  lon: z.number(),
+  lat: z.number(),
+});
+
 app.post("/", (req, res) => {
-  const { lon, lat } = req.body;
-  if (typeof lon === "number" && typeof lat === "number") {
-    socket.send(`geo fix ${lon} ${lat}`);
-    res.send("Got a POST request");
-  } else {
+  const parseResult = setCoordsRequestSchema.safeParse(req.body);
+  if (!parseResult.success) {
     res.status(400).send("Invalid coordinates");
+    return;
   }
+  const { lon, lat } = parseResult.data;
+  socket.send(`geo fix ${lon} ${lat}`);
+  res.status(200).send();
 });
 
 const server = app.listen(config.serverPort, () => {
@@ -31,10 +38,8 @@ server.on("error", (err: any) => {
         `Port ${config.serverPort} is already in use. Please choose another port.`
       );
       process.exit(1);
-      break;
     default:
       console.error(err);
       process.exit(1);
-      break;
   }
 });
